d273ba3244bf4ebc13161d90605bf8014d6e896d
lakshmimungara <lakshmimungara2997@gmail.com>
2024-09-30 18:48:52 +0530

    terraform-aws VPC module

--- outputs.tf
+++ outputs.tf
@@ -23,5 +23,9 @@ output "private_subnet_ids"{
 }
 
 output "database_subnet_ids"{
-  value  = aws_subnet.database[*].id 
+  value  = aws_subnet.database.main.name  
+}
+
+output "database_subnet_group_name"{
+  value  = aws_db_subnet_group[*].id 
 }
